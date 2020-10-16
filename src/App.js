a197f1f441fdb7b1ad2f10cc28b362bee482c314
NickHemingwayy <nickhemingway@outlook.com>
2020-10-16 09:21:51 -0700

    Removed animation and changed background

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the beginning of our Captsone project!
+          Welcome to Noggn.
         </p>
       </header>
     </div>
